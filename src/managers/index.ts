a18a0412114eb00dd36f162a76fd6afc41ab5a09
5shunchen <5shunchen@gmail.com>
2026-05-21 23:38:24 +0800

    Delete src dire1ctory
    1

--- src/managers/index.ts
+++
@@ -1,5 +0,0 @@
-export { GitManager, defaultGitManager } from './git-manager';
-export { default as ProjectManager } from './project-manager';
-export { default as EnvManager } from './env-manager';
-export { AgentManager, agentManager } from './agent-manager';
-export { default as Orchestrator, defaultOrchestrator } from './orchestrator';
